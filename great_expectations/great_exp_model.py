ec2d433e9d63e7277a0525cc369e209c02552777
Harshit Tathagat <harshittathagat28@gmail.com>
2024-11-28 14:59:02 +0530

    Logical fix in great_exp_model

--- great_expectations/great_exp_model.py
+++ great_expectations/great_exp_model.py
@@ -310,52 +310,50 @@ class GreatExpectationsModel:
         return checkpoint_result
 
 
-    @staticmethod
-    def run_quality_checks_for_db(datasource_type: str, hostname: str, password: str, username: str, 
-                                    port: int, datasource_name: str, schema_name: str, database: str, 
-                                    table_name: str, quality_checks: List[dict], batch_limit: Optional[int] = 0):
-        
-        expectation_suite_name_db = f"{datasource_name}_{username}_{table_name}_{port}_{hostname}"
-        
-        ge = GreatExpectationsModel(quality_checks=quality_checks)
-        ge_sql = ge.GE_SQL_Datasource(datasource_type=datasource_type, host=hostname, password=password, 
-                                        username=username, database=database, schema_name=schema_name, 
-                                        datasource_name=datasource_name, port=port, table_name=table_name)
-        
-        database_config_yaml = ge_sql.get_database_config()
-        ge.__create_sql_datasource(database_config_yaml=database_config_yaml)
-        ge.__create_or_load_expectation_suite(expectation_suite_name=expectation_suite_name_db)
-        batch_request_json = ge.__create_batch_request_json_for_db(datasource_name=datasource_name,
-                                                data_asset_name=table_name,
-                                                limit=batch_limit)
-        db_validator = ge.__create_validator(expectation_suite_name=expectation_suite_name_db, 
-                                            batch_request=batch_request_json)
-        ge.__add_expectations_to_validator(validator=db_validator,expectations=ge.quality_checks)
-        checkpoint_results = ge.__create_and_execute_checkpoint(expectation_suite_name=expectation_suite_name_db,
-                                                                batch_request=batch_request_json,
-                                                                validator=db_validator)
-        return checkpoint_results # TODO: add utils.find_validation_result()
+def run_quality_checks_for_db(datasource_type: str, hostname: str, password: str, username: str, 
+                                port: int, datasource_name: str, schema_name: str, database: str, 
+                                table_name: str, quality_checks: List[dict], batch_limit: Optional[int] = 0):
+    
+    expectation_suite_name_db = f"{datasource_name}_{username}_{table_name}_{port}_{hostname}"
+    
+    ge = GreatExpectationsModel(quality_checks=quality_checks)
+    ge_sql = ge.GE_SQL_Datasource(datasource_type=datasource_type, host=hostname, password=password, 
+                                    username=username, database=database, schema_name=schema_name, 
+                                    datasource_name=datasource_name, port=port, table_name=table_name)
+    
+    database_config_yaml = ge_sql.get_database_config()
+    ge.__create_sql_datasource(database_config_yaml=database_config_yaml)
+    ge.__create_or_load_expectation_suite(expectation_suite_name=expectation_suite_name_db)
+    batch_request_json = ge.__create_batch_request_json_for_db(datasource_name=datasource_name,
+                                            data_asset_name=table_name,
+                                            limit=batch_limit)
+    db_validator = ge.__create_validator(expectation_suite_name=expectation_suite_name_db, 
+                                        batch_request=batch_request_json)
+    ge.__add_expectations_to_validator(validator=db_validator,expectations=ge.quality_checks)
+    checkpoint_results = ge.__create_and_execute_checkpoint(expectation_suite_name=expectation_suite_name_db,
+                                                            batch_request=batch_request_json,
+                                                            validator=db_validator)
+    return checkpoint_results # TODO: add utils.find_validation_result()
 
 
-    @staticmethod
-    def run_quality_check_for_file(datasource_type: str, datasource_name: str, dir_name: str, quality_checks: List[dict], 
-                                file_name: str, batch_limit: Optional[int] = 0):
-        
-        expectation_suite_name_file = f"{datasource_name}_{dir_name}_{datasource_type}_{file_name}"
-        
-        ge = GreatExpectationsModel(quality_checks=quality_checks)
-        ge_file = ge.GE_File_Datasource(datasource_name=datasource_name, datasource_type=datasource_type, dir_name=dir_name)
-        
-        file_config_yaml = ge_file.get_file_config()
-        ge.__create_file_datasource(file_config_yaml=file_config_yaml)
-        ge.__create_or_load_expectation_suite(expectation_suite_name=expectation_suite_name_file)
-        batch_request_json = ge.__create_batch_request_json_for_file(datasource_name=datasource_name, 
-                                                                    data_asset_name=file_name,
-                                                                    limit=batch_limit)
-        file_validator = ge.__create_validator(expectation_suite_name=expectation_suite_name_file)
-        ge.__add_expectations_to_validator(validator=file_validator,
-                                        expectations=quality_checks)
-        checkpoint_results = ge.__create_and_execute_checkpoint(expectation_suite_name=expectation_suite_name_file,
-                                                                batch_request=batch_request_json,
-                                                                validator=file_validator)
-        return checkpoint_results # TODO: add utils.find_validation_result()
+def run_quality_check_for_file(datasource_type: str, datasource_name: str, dir_name: str, quality_checks: List[dict], 
+                            file_name: str, batch_limit: Optional[int] = 0):
+    
+    expectation_suite_name_file = f"{datasource_name}_{dir_name}_{datasource_type}_{file_name}"
+    
+    ge = GreatExpectationsModel(quality_checks=quality_checks)
+    ge_file = ge.GE_File_Datasource(datasource_name=datasource_name, datasource_type=datasource_type, dir_name=dir_name)
+    
+    file_config_yaml = ge_file.get_file_config()
+    ge.__create_file_datasource(file_config_yaml=file_config_yaml)
+    ge.__create_or_load_expectation_suite(expectation_suite_name=expectation_suite_name_file)
+    batch_request_json = ge.__create_batch_request_json_for_file(datasource_name=datasource_name, 
+                                                                data_asset_name=file_name,
+                                                                limit=batch_limit)
+    file_validator = ge.__create_validator(expectation_suite_name=expectation_suite_name_file)
+    ge.__add_expectations_to_validator(validator=file_validator,
+                                    expectations=quality_checks)
+    checkpoint_results = ge.__create_and_execute_checkpoint(expectation_suite_name=expectation_suite_name_file,
+                                                            batch_request=batch_request_json,
+                                                            validator=file_validator)
+    return checkpoint_results # TODO: add utils.find_validation_result()
