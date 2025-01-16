e4afac20a3089c786e91a98b9fa46040ca04c72a
Harshit Tathagat <harshittathagat28@gmail.com>
2025-01-16 21:52:52 +0530

    Added shell=True at line 21 in main.py for execution in prod servers

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def execute_api_request(host: str, port: int) -> None:
     :return: None
     """
     dqt_logger.info(f"Running Fast API on host: {host} on port: {port}")
-    subprocess.run(f"uvicorn fast_api:app --host {host} --port {port}")
+    subprocess.run(f"uvicorn fast_api:app --host {host} --port {port}", shell=True)
 
 def execute_standalone_script(endpoint:str, request_json: Optional[dict]=None, job_id: Optional[str]=None) -> None:
     """
